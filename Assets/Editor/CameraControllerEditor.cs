4f1f50fa19e2bf39c5ad0425212129b77eaddbef
Benny Liang <bennyliang24@gmail.com>
2026-06-23 17:49:25 -0600

    feat: Unity Custom Editor
    added a custom unity editor to the CameraController.cs

---
+++ Assets/Editor/CameraControllerEditor.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CameraController), true)]
+public class CameraControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUILayout.Space(10);
+
+        CameraController cameraController = (CameraController)target;
+        if (GUILayout.Button("Toggle Psuedo Orthographic Projection"))
+        {
+            cameraController.TogglePeseudoOrthographic();
+        }
+    }
+}
