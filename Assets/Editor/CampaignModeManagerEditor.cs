35277f4bc894299338b2b25c848479fa8ae7f385
Benny Liang <bennyliang24@gmail.com>
2026-06-25 19:57:58 -0600

    feat campaign mode manager custom unity editor

---
+++ Assets/Editor/CampaignModeManagerEditor.cs
@@ -0,0 +1,30 @@
+﻿
+using StorytellersTable.Campaign.Modes;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CampaignModeManager), true)]
+public class CampaignModeManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUILayout.Space(10);
+
+        CampaignModeManager manager = (CampaignModeManager)target;
+
+        if (GUILayout.Button("Swith to Play mode"))
+        {
+            manager.SwitchMode(CampaignModeType.Play);
+        }
+        if (GUILayout.Button("Swith to Map Edit mode"))
+        {
+            manager.SwitchMode(CampaignModeType.MapEdit);
+        }
+        if (GUILayout.Button("Swith to Entity Edit mode"))
+        {
+            manager.SwitchMode(CampaignModeType.EntityEdit);
+        }
+    }
+}
