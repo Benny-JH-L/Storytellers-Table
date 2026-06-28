0397b2d6f58dc1c0d1fee693ccdbd86ad4417ebe
Benny Liang <bennyliang24@gmail.com>
2026-06-28 15:53:08 -0600

    feat: hexrenderer custom unity editor inspector

---
+++ Assets/Editor/HexRendererEditor.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(HexRenderer), true)]
+public class HexRendererEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        HexRenderer hexRenderer = (HexRenderer)target;
+
+        if (GUILayout.Button("Redraw mesh"))
+            hexRenderer.DrawMesh();
+    }
+}
