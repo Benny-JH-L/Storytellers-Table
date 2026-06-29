bb012b667004b4b81f6d6ba8d6b31e350a0d31c2
Benny Liang <bennyliang24@gmail.com>
2026-06-29 15:10:38 -0600

    feat: singleton
    added to store resued objects

---
+++ Assets/StorytellersTable/Singleton.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// Singleton that holds data that's used across files.
+/// </summary>
+[DisallowMultipleComponent]
+public class Singleton : MonoBehaviour
+{
+    public static Singleton Instance { get; private set; }
+
+    [Header("Tile")]
+    public Material defaultTileMaterial;
+    public Material ghostTileMaterial;      // for now it will be one material, can change it in the future to reflect the specific tile type (ie snow, grass, etc.)
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+}
